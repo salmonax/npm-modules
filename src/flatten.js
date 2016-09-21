7881b383b9e5a3078d8feadb4a10a5fe7c8b01ac
lizzkats <katsnelson.lizz@gmail.com>
2016-09-21 15:53:29 -0700

    add flatten

---
+++ src/flatten.js
@@ -0,0 +1,20 @@
+const flatten = (collection) => {
+
+  let output = []
+  if (collection instanceof Array || typeof collection === 'string') {
+    for (let i=0;i < collection.length; i++) { 
+      if (collection[i] instanceof Array) {
+        let subArray = collection[i]
+        for (let j=0;j < subArray.length; j++) {
+          output.push(subArray[j])
+        }
+      } else {
+        output.push(collection[i])
+      }
+
+    }
+  }
+  return output
+}
+
+export { flatten } 
