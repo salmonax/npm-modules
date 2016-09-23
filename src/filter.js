b81049a34627d6576085933c84372fc4196168f2
lizzkats <katsnelson.lizz@gmail.com>
2016-09-23 11:40:14 -0700

    Add object predicate handling to filter, finish pending matches tests

--- src/filter.js
+++ src/filter.js
@@ -1,11 +1,22 @@
-const filter = (collection, callback) => {
+import matches from './matches'
+
+const filter = (collection, predicate) => {
   let results = [] 
-  for (let i=0; i < collection.length; i++) {
-    if (callback(collection[i])) {
-      results.push(collection[i])
+  let predicateFunction
+  if (collection instanceof Array) {
+    if (typeof predicate === 'object') {
+      predicateFunction = matches(predicate)
+    } else { 
+      predicateFunction = predicate
     }
-  }
+    for (let i=0; i < collection.length; i++) {
+      if (predicateFunction(collection[i])) {
+        results.push(collection[i])
+      }  
+    }
+  } 
   return results
 }
+
 
 export default filter
